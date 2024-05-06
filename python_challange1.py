fc4292c2384cb5114fc995bebc65f553acb42dba
eric-asam22 <60238760+eric-asam22@users.noreply.github.com>
2024-05-06 11:43:14 +0000

    testing

--- python_challange1.py
+++ python_challange1.py
@@ -4,6 +4,9 @@
 # and False otherwise. Anagrams are strings that contain the same characters but may be in a different order.
 
 def is_anagram(str1, str2):
+   if(len(str1)!=(str2)): return False
+   else
+        sorted_str1= ''.join()
 
 
 # Test the function
