9d41f07078a4a8c2d897d5e639a17059b0e6a41d
eric-asam22 <60238760+eric-asam22@users.noreply.github.com>
2024-05-06 14:04:36 +0200

    Create python_challenge.py

---
+++ python_challenge.py
@@ -0,0 +1,15 @@
+# Challenge: Anagram Check
+
+# Write the function is_anagram that takes two strings as input and returns True if the strings are anagrams of each other,
+# and False otherwise. Anagrams are strings that contain the same characters but may be in a different order.
+
+def is_anagram(str1, str2):
+    #input your code
+
+
+# Test the function
+input_str1 = "listen"
+input_str2 = "silent"
+expected_output = True
+output = is_anagram(input_str1, input_str2)
+print(output == expected_output)
